c4c74ca7e3ac60c8b7e6c8e247e10dc66516bf39
NelsonWang <wangnelson2@gmail.com>
2022-12-08 14:18:46 -0500

    cli

--- 10-CLI Projects/24-b-cli/index.js
+++ 10-CLI Projects/24-b-cli/index.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 const path = require('path')
 const copy = require('copy-template-dir')
-const vars = { name: 'awais' }
+const vars = { name: 'Nelson' }
 
+const inDir = path.join(__dirname, 'template'); // This one creates cwd relative to the CLI
+const outDir = path.join(process.cwd(), 'output'); // This prints to the current directory
+copy(inDir, outDir, vars, ((err, createdFiles) => {
+    if (err) throw err
+    createdFiles.forEach(filePath => console.log(`Created ${filePath}`))
+    console.log('done!');
 
+}))
 
 
 
